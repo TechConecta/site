41111a902e9e843ff122593cd1d4c32be8f756bb
Felipe A de Almeida <felipe.albuquerque@gruposaltaedu.com>
2023-10-23 07:04:28 -0300

    configurações para publicação no firebase

--- techconecta/src/app/app.component.ts
+++ techconecta/src/app/app.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { MenuItem } from './@thinkam/pilot/models/menu-item.model';
-
+import { environment } from '../environments/environment';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor() {
+    const firebaseConfig = environment.firebaseConfig;
+
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
+
   title(title: any) {
     throw new Error('Method not implemented.');
   }
+
   readonly logoSrc: string = 'assets/images/techconecta-logotipo.svg';
+  
   readonly menus: Array<MenuItem> = [
     {
       path: '',
